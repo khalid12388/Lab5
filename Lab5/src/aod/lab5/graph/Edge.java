a17c511c6bff66e4e91e89c7d13d04dad4935035
khalid12388 <abdis12388@gmail.com>
2026-05-12 13:31:59 +0200

    Update Edge.java

--- Lab5/src/aod/lab5/graph/Edge.java
+++ Lab5/src/aod/lab5/graph/Edge.java
@@ -3,57 +3,102 @@ package aod.lab5.graph;
 import java.awt.Color;
 
 /**
- * Represents a directed edge between two vertices in a graph.
+ * Represents a directed edge between two vertices.
  *
  * @param <T> the type of information stored in the vertices
  */
-public class Edge<T> {
+public final class Edge<T> {
 
+    /**
+     * The start vertex of the edge.
+     */
     private Vertex<T> from;
+
+    /**
+     * The end vertex of the edge.
+     */
     private Vertex<T> to;
+
+    /**
+     * The distance between the vertices.
+     */
     private double distance;
+
+    /**
+     * The color of the edge.
+     */
     private Color color;
 
     /**
-     * Creates an edge from one vertex to another.
+     * Creates a new edge between two vertices.
      *
-     * @param from the start vertex
-     * @param to the end vertex
+     * @param startVertex the start vertex
+     * @param endVertex the end vertex
      */
-    public Edge(Vertex<T> from, Vertex<T> to) {
-        this.from = from;
-        this.to = to;
-        this.distance = calculateDistance(from, to);
+    public Edge(final Vertex<T> startVertex, final Vertex<T> endVertex) {
+        this.from = startVertex;
+        this.to = endVertex;
+        this.distance = calculateDistance(startVertex, endVertex);
         this.color = Color.GRAY;
     }
 
     /**
-     * Calculates the distance between two vertices using Pythagoras.
+     * Calculates the distance between two vertices.
+     *
+     * @param startVertex the start vertex
+     * @param endVertex the end vertex
+     * @return the distance between the vertices
      */
-    private double calculateDistance(Vertex<T> from, Vertex<T> to) {
-        double dx = to.getX() - from.getX();
-        double dy = to.getY() - from.getY();
+    private double calculateDistance(final Vertex<T> startVertex,
+            final Vertex<T> endVertex) {
+        double dx = endVertex.getX() - startVertex.getX();
+        double dy = endVertex.getY() - startVertex.getY();
 
         return Math.sqrt(dx * dx + dy * dy);
     }
 
+    /**
+     * Returns the start vertex.
+     *
+     * @return the start vertex
+     */
     public Vertex<T> getFrom() {
         return from;
     }
 
+    /**
+     * Returns the end vertex.
+     *
+     * @return the end vertex
+     */
     public Vertex<T> getTo() {
         return to;
     }
 
+    /**
+     * Returns the distance of the edge.
+     *
+     * @return the distance
+     */
     public double getDistance() {
         return distance;
     }
 
+    /**
+     * Returns the edge color.
+     *
+     * @return the edge color
+     */
     public Color getColor() {
         return color;
     }
 
-    public void setColor(Color color) {
-        this.color = color;
+    /**
+     * Sets the edge color.
+     *
+     * @param newColor the new color
+     */
+    public void setColor(final Color newColor) {
+        this.color = newColor;
     }
 }
